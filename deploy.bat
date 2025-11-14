9b8ffb4a1de9d4a496e995787afc6c26e0a1702a
daishu0000 <daishu10000@gmail.com>
2025-11-14 20:18:48 +0800

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -5,8 +5,4 @@ cd dist
 git init
 git add .
 git commit -m "push to gh-pages"
-git push --force git@github.com:daishu0000/city-roads.git main:gh-pages
-cd ..
-for /f "tokens=1-4 delims=/- " %%a in ('echo %date% %time%') do set TAG=release-%%a%%b%%c%%d
-git tag %TAG%
-git push --tags
+git push https://github.com/daishu0000/city-roads.git master:gh-pages
